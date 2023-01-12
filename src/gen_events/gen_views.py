10cc1c53931cb4e138e2cbbf7ec945998be704a5
Viatcheslav Pestov <pestov74@gmail.com>
2023-01-12 23:11:22 +0500

    generating views - 2

--- src/gen_events/gen_views.py
+++ src/gen_events/gen_views.py
@@ -30,16 +30,6 @@ max_len = 3 * 60 * 60
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
-'''
-producer.send(
-    topic='views',
-    value=b'1611039931',
-    key=b'500271+tt0120338',
-)
-
-sleep(1)
-'''
-
 
 def gen_users() -> dict:
     users = dict()
