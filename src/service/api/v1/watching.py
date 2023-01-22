944f6cc702e852f56f83e53611d519a995358c18
Viatcheslav Pestov <pestov74@gmail.com>
2023-01-22 12:02:10 +0500

    add auth

--- src/service/api/v1/watching.py
+++ src/service/api/v1/watching.py
@@ -3,10 +3,13 @@ import uuid
 from uuid import UUID
 
 from aiokafka import AIOKafkaProducer
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 
 from api.v1.schemes import WatchingResponse
 from db.kafka import get_kafka_producer
+
+from auth.auth_bearer import JWTBearer
+from auth.auth_handler import encode_jwt, decode_jwt
 
 router = APIRouter()
 
@@ -14,9 +17,33 @@ logger = logging.getLogger(__name__)
 
 
 @router.post(
-    "/{movie_id}", response_model=WatchingResponse, summary="Добавление просмотренного фрейма в кафку", description=""
+    "/{movie_id}",
+    response_model=WatchingResponse,
+    summary="Добавление просмотренного фрейма в кафку",
+    description="",
+    dependencies=[Depends(JWTBearer())]
 )
-async def watched_movies(frame: str, movie_id: UUID, kafka_producer: AIOKafkaProducer = Depends(get_kafka_producer)):
+async def watched_movies(request: Request,
+                         frame: str,
+                         movie_id: UUID,
+                         kafka_producer:
+                         AIOKafkaProducer = Depends(get_kafka_producer)
+                         ):
     user = uuid.uuid4()
     await kafka_producer.send_and_wait(f"{user}{movie_id}", frame.encode("UTF-8"))
     return WatchingResponse(movie_id=movie_id, user_id=user, frame=frame)
+
+@router.get(
+    "/access_token",
+    response_model=None,
+    summary="",
+    description="",
+    response_description="",
+    tags=["events"],
+)
+async def get_access_token(user_id: str | None = None
+) -> str:
+    if not user_id:
+        user_id = str(uuid.uuid4())
+    token: str = encode_jwt(user_id)
+    return token
