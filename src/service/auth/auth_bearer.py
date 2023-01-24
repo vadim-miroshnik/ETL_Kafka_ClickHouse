ab5e3ab3deb926b9eb3401bbede4de7b5c869960
Vyacheslav Pestov <v_pestov@hotmail.com>
2023-01-24 08:49:00 +0500

    fix 4

--- src/service/auth/auth_bearer.py
+++ src/service/auth/auth_bearer.py
@@ -7,7 +7,7 @@ from .auth_handler import decode_jwt
 
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
-        super(JWTBearer, self).__init__(auto_error=auto_error)
+        super().__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request):
         credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
